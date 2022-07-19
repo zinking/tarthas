327996412006354b0de3f5c1c906513e43627735
zhnwang <zhnwang@tencent.com>
2022-07-19 17:03:54 +0800

    clean up unnecessary components

---
+++ tarthas/src/main/java/arthas/ArthasCapture.java
@@ -0,0 +1,14 @@
+package arthas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArthasCapture {
+  public static List<Object> capturedObjs = new ArrayList<>();
+
+  public static String capture(Object obj) {
+    capturedObjs.add(obj);
+    return "1";
+  }
+
+}
